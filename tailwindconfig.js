63213cc319324d0116b2a8deb9361de9aa25604a
alessandrojunior1 <130919708+alessandrojunior1@users.noreply.github.com>
2023-10-23 02:50:02 -0300

    primeiro commit

---
+++ tailwindconfig.js
@@ -0,0 +1,21 @@
+tailwind.config = {
+    theme: {
+        extend:{
+      colors: {
+        'esverdeado': '#1D9E6A',
+        'azulado': '#608E83',
+        'amarelo': '#FFF500',
+        'azul': '#2D3580',
+        'vermelho' : '#CD1111',
+        'verde' : '#2AC239'
+      },
+      fontFamily: {
+        'roboto': ['roboto slab', 'serif']
+      },
+      boxShadow:{
+        'sus': '0 3px 60px 10px rgb(29, 158, 106, /100%)'
+      }
+      
+    }
+  }
+}
