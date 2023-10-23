53185e8eede6a95aec7af375853798bc86173ccb
Alex Loureiro Soares <128738123+SunsettingAlex@users.noreply.github.com>
2023-10-23 15:23:54 -0300

    Modificação das imagens e mudança do src no data.js

--- js/data.js
+++ js/data.js
@@ -1,32 +1,39 @@
 export const lixeiras = [
-    {
-      name: 'Lixeira Vermelha',
-      type: 'Plástico',
-      about: 'Lixeira para Plástico',
+  {
+    name: 'Lixeira Vermelha',
+    type: 'Plástico',
+    about: 'Lixeira para Plástico',
+    date: '2023-03-22',
+    image: '"imgs/vermelho.jpg" alt="latavermelha"'
+  },
+  {
+      name: 'Lixeira Verde',
+      type: 'Vidro',
+      about: 'Lixeira para Vidro',
       date: '2023-03-22',
-      image: '"imgs/lata_vermelha-removebg-preview 1.png" alt="latavermelha"'
+      image: '"imgs/verde.jpg" alt="lataverde"'
     },
     {
-        name: 'Lixeira Verde',
-        type: 'Vidro',
-        about: 'Lixeira para Vidro',
-        date: '2023-03-22',
-        image: '"imgs/lata_verde-removebg-preview 1.png" alt="lataverde"'
-      },
-      {
-        name: 'Lixeira Azul',
-        type: 'Papel',
-        about: 'Lixeira para Papel',
-        date: '2023-03-22',
-        image: '"imgs/lata_azul-removebg-preview 1.png" alt="lataazul"'
-      },
-      {
-        name: 'Lixeira Amarela',
-        type: 'Metal',
-        about: 'Lixeira para Metal',
-        date: '2023-03-22',
-        image: '"imgs/lata_amarela-removebg-preview 1.png" alt="lataamarela"'
-      },
+      name: 'Lixeira Azul',
+      type: 'Papel',
+      about: 'Lixeira para Papel',
+      date: '2023-03-22',
+      image: '"imgs/azul.jpg" alt="lataazul"'
+    },
+    {
+      name: 'Lixeira Amarela',
+      type: 'Metal',
+      about: 'Lixeira para Metal',
+      date: '2023-03-22',
+      image: '"imgs/amarelo.jpg" alt="lataamarela"'
+    },
+    {
+      name: 'Lixeira Marrom',
+      type: 'Orgânico',
+      about: 'Lixeira para Orgânico',
+      date: '2023-03-22',
+      image: '"imgs/marrom.jpg" alt="latamarrom"'    
+    }
       // {
       //   name: 'Lixeira Marrom',
       //   type: 'Orgânico',
