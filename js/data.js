28687a5cbc881da29e8c58912ff6557cacfc37a1
SunsetAlex <heroofmito@gmail.com>
2023-10-24 23:06:03 -0300

    mais mudanças no filter

--- js/data.js
+++ js/data.js
@@ -3,35 +3,35 @@ export const lixeiras = [
     name: 'Lixeira Vermelha',
     type: 'Plástico',
     about: 'Lixeira para Plástico',
-    itens_descartaveis:['Plástico','Plastico','Plastificado'],
+    itens_descartaveis:['Plástico','Plastico','Plastificado','Copo de Plástico','Copo plástico','Balde','Garrafa Pet'],
     image: '"imgs/vermelho.jpg" alt="latavermelha"'
   },
   {
       name: 'Lixeira Verde',
       type: 'Vidro',
       about: 'Lixeira para Vidro',
-      itens_descartaveis:['Vidro','Porcelana','Vidraça','Cacos'],
+      itens_descartaveis:['Vidro','Vidraça','Cacos','Garrafa de Vidro'],
       image: '"imgs/verde.jpg" alt="lataverde"'
     },
     {
       name: 'Lixeira Azul',
       type: 'Papel',
       about: 'Lixeira para Papel',
-      itens_descartaveis:['Papel','Papelao','Papiro','Folha'],
+      itens_descartaveis:['Papel','Papelao','Papelão','Papiro','Folha','Copo de Papel','Refrigerante'],
       image: '"imgs/azul.jpg" alt="lataazul"'
     },
     {
       name: 'Lixeira Amarela',
       type: 'Metal',
       about: 'Lixeira para Metal',
-      itens_descartaveis:['Metal','Laminado','Aluminio','Alumínio','Lata','Latão','Enlatado'],
+      itens_descartaveis:['Metal','Laminado','Aluminio','Alumínio','Lata','Latão','Enlatado','Refrigerante'],
       image: '"imgs/amarelo.jpg" alt="lataamarela"'
     },
     {
       name: 'Lixeira Marrom',
       type: 'Orgânico',
       about: 'Lixeira para Orgânico',
-      itens_descartaveis:['Orgânico','Organico','Comida','Comida','Restos de comida'],
+      itens_descartaveis:['Orgânico','Organico','Comida','Restos de comida','Pipoca'],
       image: '"imgs/marrom.jpg" alt="latamarrom"'    
     },
     {
