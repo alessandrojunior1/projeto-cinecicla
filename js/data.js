83c30285265198033a615b0a3a77a22c40debad0
SunsetAlex <heroofmito@gmail.com>
2023-10-24 22:49:55 -0300

    nomes para filtro

--- js/data.js
+++ js/data.js
@@ -3,42 +3,42 @@ export const lixeiras = [
     name: 'Lixeira Vermelha',
     type: 'Plástico',
     about: 'Lixeira para Plástico',
-    itens_descartaveis:['teste1','hbgfsdk'],
+    itens_descartaveis:['Plástico','Plastico','Plastificado'],
     image: '"imgs/vermelho.jpg" alt="latavermelha"'
   },
   {
       name: 'Lixeira Verde',
       type: 'Vidro',
       about: 'Lixeira para Vidro',
-      itens_descartaveis:['teste3','asdasgfsd'],
+      itens_descartaveis:['Vidro','Porcelana','Vidraça','Cacos'],
       image: '"imgs/verde.jpg" alt="lataverde"'
     },
     {
       name: 'Lixeira Azul',
       type: 'Papel',
       about: 'Lixeira para Papel',
-      itens_descartaveis:['papel','papelao'],
+      itens_descartaveis:['Papel','Papelao','Papiro','Folha'],
       image: '"imgs/azul.jpg" alt="lataazul"'
     },
     {
       name: 'Lixeira Amarela',
       type: 'Metal',
       about: 'Lixeira para Metal',
-      itens_descartaveis:['c1','c2'],
+      itens_descartaveis:['Metal','Laminado','Aluminio','Alumínio','Lata','Latão','Enlatado'],
       image: '"imgs/amarelo.jpg" alt="lataamarela"'
     },
     {
       name: 'Lixeira Marrom',
       type: 'Orgânico',
       about: 'Lixeira para Orgânico',
-      itens_descartaveis:['papel','abc2'],
+      itens_descartaveis:['Orgânico','Organico','Comida','Comida','Restos de comida'],
       image: '"imgs/marrom.jpg" alt="latamarrom"'    
     },
     {
       name: 'Lixeira Cinza',
       type: 'Não-Reciclável',
       about: 'Lixeira para Não-Recicláveis',
-      itens_descartaveis:['abc','abc'],
+      itens_descartaveis:['Não-reciclável','Nao-reciclavel','Não reciclável','Nao reciclavel'],
       image: '"imgs/cinza.png" alt="latacinza"'    
     },
   ];
